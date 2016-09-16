c76a2406f249872cdbae6ff21eb494c68e8b7625
Javier Lopez <javy7@hotmail.com>
2016-09-15 23:42:45 -0400

    Code changes :p

--- app/src/main/java/com/learningandroid/justjava/MainActivity.java
+++ app/src/main/java/com/learningandroid/justjava/MainActivity.java
@@ -18,46 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int quantity = 1;
-    /**
-     * This method is called when the order button is clicked
-     */
-    public void submitOrder(View view)
-    {
-        displayPrice(quantity * 5);
-
-//        EditText quantityEditText = (EditText) findViewById(R.id.quantity_edit_text);
-//
-//        if (numberOfCoffees > 0)
-//        {
-//            numberOfCoffees = Integer.parseInt(quantityEditText.getText().toString());
-//            display(numberOfCoffees);
-
-//        }
-//        else
-//        {
-//            Toast.makeText(getApplicationContext(), "Please enter quantity", Toast.LENGTH_SHORT).show();
-//        }
-    }
-
-
-    /**
-     * This method displays the given quantity value on the screen
-     */
-    private void display(int number)
-    {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price value on the screen.
-     */
-    private void displayPrice(int number)
-    {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+    int quantity = 2;
 
     /**
      * Method to increment the number of cups
@@ -77,5 +38,47 @@ public class MainActivity extends AppCompatActivity {
     {
         quantity = quantity - 1;
         display(quantity);
+    }
+
+
+    /**
+     * This method is called when the order button is clicked
+     */
+    public void submitOrder(View view)
+    {
+        //displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage = "The price for " + quantity + " cup(s) is $" + price;
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen
+     */
+    private void display(int number)
+    {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price value on the screen.
+     */
+    private void displayPrice(int number)
+    {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
+
+    /**
+     * This method displays the given text on the screen.
+     * @param message
+     */
+    private void displayMessage(String message)
+    {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
